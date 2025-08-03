c5b45f55c03c787ca5ec0deaccd6734489e5a5bf
NamhyeonKo <glucosamin7@naver.com>
2025-08-03 09:45:59 +0900

    create ethernet structure and function

--- ethhdr.cpp
+++ ethhdr.cpp
@@ -1,1 +1,19 @@
 #include "ethhdr.h"
+
+u_int8_t* stringmac_to_bytemac(char* str_mac){
+    u_int8_t byte_mac[6] = {0, 0, 0, 0, 0, 0};
+    sscanf(str_mac,
+        "%hhX:%hhX:%hhX:%hhX:%hhX:%hhX",
+        &byte_mac[0], &byte_mac[1], &byte_mac[2], &byte_mac[3], &byte_mac[4], &byte_mac[5]
+    );
+    return byte_mac;
+}
+
+char* bytemac_to_stringmac(u_int8_t* byte_mac){
+    char str_mac[18] = {0};
+    sprintf(str_mac,
+        "%02X:%02X:%02X:%02X:%02X:%02X",
+        byte_mac[0], byte_mac[1], byte_mac[2], byte_mac[3], byte_mac[4], byte_mac[5]
+    );
+    return str_mac;
+}
--- ethhdr.h
+++ ethhdr.h
@@ -1,8 +1,12 @@
 #pragma once
 #include <stdio.h>
+#include <stdint.h>
 
 typedef struct MY_ETHERNET_HEADER{	/* ethernet_hdr total 14bytes */
     u_int8_t  ether_dhost[6];	/* destination ethernet address */
     u_int8_t  ether_shost[6];	/* source ethernet address */
     u_int16_t ether_type;	/* protocol */
 } ETHERNET_HDR;
+
+u_int8_t* stringmac_to_bytemac(char* str_mac);
+char* bytemac_to_stringmac(u_int8_t* byte_mac);
